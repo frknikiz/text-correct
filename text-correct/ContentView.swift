c0c981da48afa6dc10aeaedebaec118ff9984f30
Furkan <frknikiz@gmail.com>
2026-01-10 18:12:43 +0300

    Initial Commit

---
+++ text-correct/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  text-correct
+//
+//  Created by frkn on 10.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ text-correct/text_correctApp.swift
@@ -0,0 +1,17 @@
+//
+//  text_correctApp.swift
+//  text-correct
+//
+//  Created by frkn on 10.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct text_correctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
